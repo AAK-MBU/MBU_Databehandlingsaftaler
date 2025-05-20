19cbbdd483e8707baf7884e74df2509b28169ebd
Sebastian <ahss@aarhus.dk>
2025-05-20 09:14:57 +0200

    Store overview as formatted eccel

--- robot_framework/subprocesses/overview_creation.py
+++ robot_framework/subprocesses/overview_creation.py
@@ -1,10 +1,13 @@
 """This module handles creation of overview"""
 import json
 import os
+from datetime import datetime
 import pandas as pd
 from tqdm import tqdm
 from requests import Session
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
+from openpyxl.worksheet.datavalidation import DataValidation
+
 from robot_framework.subprocesses.helper_functions import (
     open_stil_connection,
     get_base_cookies,
@@ -16,6 +19,42 @@ from robot_framework.subprocesses.helper_functions import (
     flatten_dict
 )
 from robot_framework.exceptions import ResponseError
+
+
+def store_overview(agreements_df: pd.DataFrame, base_dir: str):
+    """Function to store overview as excel"""
+    agreements_df["statusændring"] = ""
+    cols_left = ["inst_kode", "inst_navn", "aktuelStatus", "statusændring"]
+    agreements_df = agreements_df[cols_left + [c for c in agreements_df.columns if c not in cols_left]]
+    filename = os.path.join(base_dir, f"dataaftaler_oversigt_{datetime.now().strftime("%d%m%Y")}.xlsx")
+
+    with pd.ExcelWriter(filename, engine='openpyxl') as writer:
+        agreements_df.to_excel(writer, index=False, sheet_name='Oversigt')
+        worksheet = writer.sheets['Oversigt']
+        worksheet.auto_filter.ref = worksheet.dimensions
+
+        for row in range(2, worksheet.max_row + 1):
+            status_cell = worksheet[f'C{row}']
+            statusændring_cell = worksheet[f'D{row}']  # 'statusændring' is in column D
+            if status_cell.value != "SLETTET":
+                dv = DataValidation(type="list", formula1='"GODKEND, SLET, VENT"')
+                dv.error_title = 'Invalid input'
+                dv.error_message = 'Please select a value from the dropdown list'
+                worksheet.add_data_validation(dv)
+                dv.add(statusændring_cell)
+
+        # Adjust column widths but limit the max width
+        max_column_width = 30
+        for col in worksheet.columns:
+            max_length = 0
+            column = col[0].column_letter
+            for cell in col:
+                try:
+                    max_length = max(max_length, len(str(cell.value)))
+                except (TypeError, ValueError):
+                    pass
+            adjusted_width = min(max_length + 2, max_column_width)
+            worksheet.column_dimensions[column].width = adjusted_width
 
 
 def run_overview_creation(orchestrator_connection: OrchestratorConnection):
@@ -48,6 +87,7 @@ def run_overview_creation(orchestrator_connection: OrchestratorConnection):
 
     # Get agreements from each organization
     orchestrator_connection.log_trace(f"Fetching agreements from {len(org_dict.keys())} institutions")
+    print(f"Fetching agreements from {len(org_dict.keys())} institutions")
     for k, v in tqdm(org_dict.items()):
         queue_element = {"Organisation": v["type"], "Instregnr": v["kode"]}
         org_response = get_org(orchestrator_connection, queue_element, runtime_args, session)
@@ -70,7 +110,7 @@ def run_overview_creation(orchestrator_connection: OrchestratorConnection):
     # Store all agreements in directory
     agreements_df = pd.DataFrame(all_agreements)
     base_dir = oc_args["base_dir"]
-    filename = os.path.join(base_dir, "alle_dataaftaler.csv")
-    agreements_df.to_csv(filename, index=None)
+    print(f"{len(all_agreements)} agreements fetched. Storing in {base_dir}")
+    store_overview(agreements_df, base_dir)
 
     orchestrator_connection.log_trace(f"{len(all_agreements)} dataaftaler fetched and stored in {base_dir}")
