518edccec42f016ad0ae2d2b4156e44a7158b25b
Sebastian <ahss@aarhus.dk>
2026-02-18 10:13:38 +0100

    Linting

--- robot_framework/queue_framework.py
+++ robot_framework/queue_framework.py
@@ -3,22 +3,20 @@
 # This module is not meant to exist next to linear_framework.py in production:
 # pylint: disable=duplicate-code
 
-import sys
 import json
+import sys
 
-from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 from OpenOrchestrator.database.queues import QueueStatus
+from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 
-from robot_framework import initialize
-from robot_framework import reset
-from robot_framework.exceptions import handle_error, BusinessError, log_exception
-from robot_framework import process
-from robot_framework import config
+from robot_framework import config, initialize, process, reset
+from robot_framework.exceptions import BusinessError, handle_error, log_exception
 
 
 def main():
     """The entry point for the framework. Should be called as the first thing when running the robot."""
     orchestrator_connection = OrchestratorConnection.create_connection_from_args()
+
     sys.excepthook = log_exception(orchestrator_connection)
 
     orchestrator_connection.log_trace("Robot Framework started.")
@@ -30,7 +28,7 @@ def main():
 
     # Run only when process
     oc_args = json.loads(orchestrator_connection.process_arguments)
-    if oc_args['process'] == "handle_queue":
+    if oc_args["process"] == "handle_queue":
         # Retry loop
         for _ in range(config.MAX_RETRY_COUNT):
             try:
@@ -39,7 +37,9 @@ def main():
                 # Queue loop
                 while task_count < config.MAX_TASK_COUNT:
                     task_count += 1
-                    queue_element = orchestrator_connection.get_next_queue_element(config.QUEUE_NAME)
+                    queue_element = orchestrator_connection.get_next_queue_element(
+                        config.QUEUE_NAME
+                    )
 
                     if not queue_element:
                         orchestrator_connection.log_info("Queue empty.")
@@ -47,10 +47,17 @@ def main():
 
                     try:
                         process.process(orchestrator_connection, queue_element)
-                        orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.DONE)
+                        orchestrator_connection.set_queue_element_status(
+                            queue_element.id, QueueStatus.DONE
+                        )
 
                     except BusinessError as error:
-                        handle_error("Business Error", error, queue_element, orchestrator_connection)
+                        handle_error(
+                            "Business Error",
+                            error,
+                            queue_element,
+                            orchestrator_connection,
+                        )
 
                 break  # Break retry loop
 
@@ -58,7 +65,12 @@ def main():
             # pylint: disable-next = broad-exception-caught
             except Exception as error:
                 error_count += 1
-                handle_error(f"Process Error #{error_count}", error, queue_element, orchestrator_connection)
+                handle_error(
+                    f"Process Error #{error_count}",
+                    error,
+                    queue_element,
+                    orchestrator_connection,
+                )
 
     reset.clean_up(orchestrator_connection)
     reset.close_all(orchestrator_connection)
